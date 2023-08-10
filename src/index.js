c98de8362d97ad65034f96a353c3a0a24e439f82
aaron roberts <roberts.am@outlook.com>
2023-08-10 14:39:37 +0100

    oop principle learnings.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { exampleLink } from './example';
 import { exampleView } from './example';
 import { exampleViewVisible } from './example';
 
-
+//
 
 export const content = document.querySelector('#content')
 export const main = document.querySelector('#main')
@@ -45,6 +45,47 @@ returnBtn.addEventListener('click', () => {
     returnBtnVisible()
 })
 
+function createViewNoteTitle(view) {
+    const noteTitle = document.createElement('h4')
+    noteTitle.classList.add('noteTitle')
+    noteTitle.innerHTML = 'notes'
+    view.appendChild(noteTitle)
+}
+
+function createViewNoteImage(imageSrc, view) {
+    const noteImage = document.createElement('img')
+    noteImage.classList.add('noteImage')
+    noteImage.src = imageSrc
+    view.appendChild(noteImage)
+}
+
+function createViewCodeTitle(view) {
+    const codeTitle = document.createElement('h4')
+    codeTitle.classList.add('codeTitle')
+    codeTitle.innerText = 'code activity'
+    view.appendChild(codeTitle)
+}
+
+function createViewCodeLog(log, view) {
+    const codeLog = document.createElement('h4')
+    codeLog.classList.add('codeLog')
+    codeLog.innerText = log
+    view.appendChild(codeLog)
+}
+
+function createCommitMade(date, view) {
+    const commitMade = document.createElement('h4')
+    commitMade.classList.add('commitMade')
+    commitMade.innerText = `commit made: ${date}`
+    view.appendChild(commitMade)
+}
+
+export {createViewNoteTitle}
+export {createViewNoteImage}
+export {createViewCodeTitle}
+export {createViewCodeLog}
+export {createCommitMade}
+
 //
 
 appendOopPrinciplesTab()
@@ -58,6 +99,7 @@ oopPrinciplesLink.addEventListener('click', () => {
     returnBtnVisible()
 })
 
+//
 
 appendExampleTab()
 appendExampleView()
--- src/oop-principles.js
+++ src/oop-principles.js
@@ -1,15 +1,17 @@
 import { tabs } from "."
 import { views } from "."
-import { returnBtn } from "."
+import { createViewNoteTitle } from "."
+import { createViewNoteImage } from "."
+import { createViewCodeTitle } from "."
+import { createViewCodeLog } from "."
+import { createCommitMade } from "."
 
 const oopPrinciplesTab = document.createElement('div')
 oopPrinciplesTab.classList.add('oopPrinciplesTab')
 
 export const oopPrinciplesLink = document.createElement('h4')
 
-function appendOopPrinciplesTab() {
-    
-    tabs.appendChild(oopPrinciplesTab)
+function createOopPrinciplesTabUI() {
 
     const oopPrinciplesPost = document.createElement('div')
     oopPrinciplesPost.classList.add('post', 'oopPrinciplesPost')
@@ -20,11 +22,15 @@ function appendOopPrinciplesTab() {
     oopPrinciplesDate.innerText = '09.08.23'
     oopPrinciplesPost.appendChild(oopPrinciplesDate)
 
-    
     oopPrinciplesLink.classList.add('link')
     oopPrinciplesLink.innerText = 'oop principles'
     oopPrinciplesPost.appendChild(oopPrinciplesLink)
 
+}
+
+function appendOopPrinciplesTab() {
+    createOopPrinciplesTabUI()
+    tabs.appendChild(oopPrinciplesTab)
 }
 
 export const oopPrinciplesView = document.createElement('div')
@@ -33,16 +39,41 @@ oopPrinciplesView.setAttribute('view-visible', 'false')
 export let oopPrinciplesViewVisible = oopPrinciplesView.getAttribute('view-visible')
 oopPrinciplesView.style.display = 'none'
 
+function createOopPrinciplesView() {
+
+    const oopPrinciplesHeader = document.createElement('div')
+    oopPrinciplesHeader.classList.add('oopPrinciplesHead', 'post')
+    oopPrinciplesView.prepend(oopPrinciplesHeader)
+    
+    const oopPrinciplesHeaderDate = document.createElement('h4')
+    oopPrinciplesHeaderDate.classList.add('date')
+    oopPrinciplesHeaderDate.innerText = '09.08.23'
+    oopPrinciplesHeader.appendChild(oopPrinciplesHeaderDate)
+
+    const oopPrinciplesHeaderTitle = document.createElement('h4')
+    oopPrinciplesHeaderTitle.classList.add('title')
+    oopPrinciplesHeaderTitle.innerText = 'oop principles'
+    oopPrinciplesHeader.appendChild(oopPrinciplesHeaderTitle)
+
+}
+
+function createOopPrinciplesNotes() {
+    createViewNoteTitle(oopPrinciplesView)
+    createViewNoteImage('/src/noteImages/oopPrinciplesNotes.png', oopPrinciplesView)
+}
+
+let oopPrinciplesLog = 'today i learnt over \'object orientated programming\'. i went on to refactor the code on this project. previously there was one function each that would create the elements for  a \'tab\' and a \'view\' and then append them to the correct elements. now there are separate function to do these two jobs, one creates the elements of the new view and tab, the other appends them.'
+
+const oopPrinciplesCommitDate = '10.08.23'
+
 function appendOopPrinciplesView() {
-
-    
+    // always this order to stack correctly
+    createOopPrinciplesNotes()
+    createOopPrinciplesView()
+    createViewCodeTitle(oopPrinciplesView)
+    createViewCodeLog(oopPrinciplesLog, oopPrinciplesView)
+    createCommitMade(oopPrinciplesCommitDate, oopPrinciplesView)
     views.appendChild(oopPrinciplesView)
-
-    const asd = document.createElement('h1')
-    asd.innerText = 'oop principles'
-    oopPrinciplesView.appendChild(asd)
-    
-
 }
 
 export {appendOopPrinciplesTab}
