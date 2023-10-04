3cd864aa938caa61bfe7dc59adb5107c24f5cd47
aaron roberts <roberts.am@outlook.com>
2023-10-05 00:14:59 +0100

    learing 05.10.23

--- src/index.js
+++ src/index.js
@@ -112,12 +112,13 @@ function Task(taskTabDate, taskTabTitle, taskViewLink, linkHref, status) {
 
 }
 
-export function Blog(tabType, tabDate, tabTitle, blogPost) {
+export function Blog(tabType, tabDate, tabTitle, blogPost, blogLink) {
 
     this.tabType = tabType
     this.tabDate = tabDate
     this.tabTitle = tabTitle
     this.blogPost = blogPost
+    this.blogLink = blogLink
 
     const blogTab = document.createElement('div')
     blogTab.classList.add('tab')
@@ -177,6 +178,17 @@ export function Blog(tabType, tabDate, tabTitle, blogPost) {
         blogPost.classList.add('learningPost')
         blogPost.innerText = this.blogPost
         blogView.appendChild(blogPost)
+
+        const blogLink = document.createElement('a');
+        blogLink.classList.add('learningPost')
+        blogLink.innerText = this.blogLink
+        blogLink.href = this.blogLink
+        blogView.appendChild(blogLink)
+
+        if (blogLink.innerText == 'none') {
+            blogLink.style.display = 'none'
+        }
+        
 
         return blogView
 
@@ -261,10 +273,19 @@ export function Welcome(tabTitle, blogPost) {
 
 
 
-const reactOnePost = "due to the sheer number of times its requested on job applications, today i started to learn react. understanding libraries and frameworks seems like a big step forward in terms of learning web development so i’m excited and eager to get into it. \n\n main takeaways\n. setting up a react environment using vite \n. introduction to react components, what they are and why they are beneficial to developers.\n. how to create and import new components into main.jsx\n. what jsx is and the differences to html\n\nfrom my limited knowledge of react, i like the idea around separating code into components to keep it as clean and readable as possible. i feel like i already attempt to do this when i write css. usually i create scss files for the header section of a website and another for the main etc etc. i find this helps when navigating files.\n\nmore of a deep dive into jsx next i think.\n\nthanks for reading,\naaron"
-const reactOne = new Blog ('learning', '01.10.23', 'react intro', reactOnePost)
-
+const reactOnePost = "due to the sheer number of times its requested on job applications, today i started to learn react. understanding libraries and frameworks seems like a big step forward in terms of learning web development so i’m excited and eager to get into it. \n\n main takeaways\n. setting up a react environment using vite. \n. introduction to react components, what they are and why they are beneficial to developers.\n. how to create and import new components into main.jsx.\n. what jsx is and the differences to html.\n\nfrom my limited knowledge of react, i like the idea around separating code into components to keep it as clean and readable as possible. i feel like i already attempt to do this when i write css. usually i create scss files for the header section of a website and another for the main etc etc. i find this helps when navigating files.\n\nmore of a deep dive into jsx next i think.\n\nthanks for reading,\naaron"
+const reactOne = new Blog ('learning', '01.10.23', 'react intro', reactOnePost, 'none')
 reactOne.appendBlog(reactOne.createBlogTab(), reactOne.createBlogView())
+
+const reactTwoPost = "i’ve continued to learn react. i’ve started to recreate past projects utilising react to do this. \n\n when writing react components one of the things i spend most to planning is what should be included in a single react component. let me try to explain this further in an example I’ve written. \n\n when creating the header section of a website, a simple set up would maybe be a website logo and a nav section. within the nav section there would be x about of list items. one question i have been asking myself is should this all be made under one component or separate this into multiple components to be housed be an overarching header component. \n\n i will link to a extremely simple header section i have quickly made. \n\n thanks for reading \n aaron"
+const reactTwo = new Blog ('learning', '03.10.23', 'creating a simple header with react', reactTwoPost, 'https://learning031023.aaronroberts.dev/')
+reactTwo.appendBlog(reactTwo.createBlogTab(), reactTwo.createBlogView())
+
+const reactThreePost = "over the past couple days i feel like i’ve made some pretty good steps forward with react. i set out to create a simple header section of a website a give it some minor functionality in a dropdown menu. \n\nthis led me to dive more into usestate hooks and passing functionality and variables between components using props.\n\ntook me some time but got there in the end. will work on making it responsive with a mobile view next.\n\nthanks for reading,\naaron"
+const reactThree = new Blog ('learning', '05.10.23', 'creating a simple header with dropdown menu functionality with react', reactThreePost, 'https://learning031023.aaronroberts.dev/')
+reactThree.appendBlog(reactThree.createBlogTab(), reactThree.createBlogView())
+
+//
 
 const welcomePost = "hi there, \n\n welcome to my learnings repository! \n\n this is a place where i can show what i've been learning, what i've been creating and some thoughts of mine throughout my journey. please have a look around to see the programming side of me. \n\n p.s the return button is in the top right hand corner." 
 const welcome = new Welcome('welcome', welcomePost)
