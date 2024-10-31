e739a9d19d78d14befdf6c10470cd42c7117e040
aaron roberts <roberts.am@outlook.com>
2024-10-31 23:09:24 +0000

    log011124.

---
+++ src/logs/2024/novemberLogs.js
@@ -0,0 +1,16 @@
+import { LogPost } from "../../script.js"
+
+const tasks = [
+    {
+        task: 'Clicking on Li element on the home page bug fix.',
+        complete: true
+    }, 
+    {
+        task: 'Complete the schema template.',
+        complete: true
+    }, 
+
+]
+
+export const log011124 = new LogPost('01.11.24', "I have the first full collection of an item arriveing tommorow morning. Site is looking good and fully set up to now just be adding the data to the database. Some small bug fixes today then will continue addding more indepth components to the site later.", tasks
+)
