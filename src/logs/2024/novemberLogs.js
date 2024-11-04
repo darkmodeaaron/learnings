927af52c169d0ef8e83657247771983f328e0777
aaron roberts <roberts.am@outlook.com>
2024-11-04 22:51:06 +0000

    log041124.

--- src/logs/2024/novemberLogs.js
+++ src/logs/2024/novemberLogs.js
@@ -14,3 +14,15 @@ const tasks = [
 
 export const log011124 = new LogPost('01.11.24', "I have the first full collection of an item arriveing tommorow morning. Site is looking good and fully set up to now just be adding the data to the database. Some small bug fixes today then will continue addding more indepth components to the site later.", tasks
 )
+
+const tasks041124 = [
+    {
+        task: 'Complete guide on how items will be measured.',
+        complete: true
+    }, 
+
+
+]
+
+export const log041124 = new LogPost('01.11.24', "No coding today but will be completing a guride I can refer to so that I get acurate info on all products consittently.", tasks041124
+)
